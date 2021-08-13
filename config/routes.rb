d01e52a0f28f80f236f54dd96f73581a743567bd
Derrick Fonseca <xracer6@hotmail.com>
2021-08-13 15:10:45 -0400

    Basic Controller and route paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/intro', to: 'homes#index'
+
+
+  namespace :api do
+    namespace :v1 do
+      resources :intro, only: [:index, :show]
+    end
+  end
 end
