25200eda51f402f945e70ec14b22e5b72bc42c51
Derrick Fonseca <xracer6@hotmail.com>
2021-08-23 17:15:45 -0400

    Seed file edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,17 @@ track1 = Track.create({name: 'Indianapolis', location: 'Indiana USA', track_type
 
 track2 = Track.create({name: 'Circuito Internacional de Algarve', location: 'Portugal', track_type: 'circuit', length: 4.8, corners: 15, description: "Challenging modern circuit with blind crests and plenty of areas to throw off racers."})
 
+track3 = Track.create({name: 'Laguna Seca', location: 'USA', track_type: 'circuit', length: 2.3, corners: 12, description: "Tight, flowing, and challenging. Not for new racers."})
+
+track4 = Track.create({name: 'Suzuka', location: 'Japan', track_type: 'circuit', length: 8.3, corners: 15, description: "Fast modern, yet classic circuit in a unique figure 8 format. Great for all vehicle types."})
+
+track5 = Track.create({name: 'Limerock Park', location: 'USA', track_type: 'circuit', length: 1.50, corners: 7, description: 'The Bull ring! A short yet intense lap where mistakes are punished heavily.'})
+
+track6 = Track.create({name: 'Dayona International Speedway', location: 'USA', track_type: 'oval', length: 2.5, corners: 7, description: 'One of the most iconic oval tracks in the entire world. Fast, banked, and iconic!'})
+
+
+
+
 event1 = Event.create({title: 'Track Daze', location_id: 1, organizer_id: 1, attendees_id: 2, description: "Open lapping/ test and tune."})
+
+event2 = Event.create({title: 'Miata Masters', location_id: 3, organizer_id: 2, attendees_id: 2, description: "MX5 Cup Masters. The best in the category meet here."})
