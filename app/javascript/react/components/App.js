6ba1aad0c1b29694808ee0fd70f1f154c60533a1
Derrick Fonseca <xracer6@hotmail.com>
2021-08-27 17:22:00 -0400

    Set up on app.js

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Intro from './Intro'
 import EventIndex from './EventIndex'
