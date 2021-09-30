01611ec7cb6503b9bc04c772a2e337acb56c32b8
Derrick Fonseca <xracer6@hotmail.com>
2021-09-29 23:31:37 -0400

    Fetch data essentials

--- app/javascript/react/components/EventIndex.js
+++ app/javascript/react/components/EventIndex.js
@@ -2,27 +2,35 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 
 const EventsIndex = (props) => {
-    const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([])
 
-      const getEvents = async () => {
-        try {
-          const response = await fetch('/api/v1/jobs')
-          if (!response) {
-            const errorMessage = `${response.status} (${response.statusTest})`
-            const error = new Error(errorMessage)
-            throw(error)
-          }
-          const parsedJobs= await response.json()
-          setJobs(parsedJobs)
-        } catch(err){
-          console.error(`Error in fetch: ${err.message}`)
-        }
+  const getEvents = async () => {
+    try {
+      const response = await fetch('/api/v1/events')
+      if (!response) {
+        const errorMessage = `${response.status} (${response.statusTest})`
+        const error = new Error(errorMessage)
+        throw(error)
       }
+      const parsedJobs= await response.json()
+      setJobs(parsedEvents)
+    } 
+    catch(err){
+      console.error(`Error in fetch: ${err.message}`)
+    }
+  }
 
+  const eventList = events.map(eachEvent => {
+    return (
+      <li key={eachEvent.title}>
 
-  return(
-    <div> *Map through each here* </div>
-  )
+      </li>
+    )
+  })
+
+  return(eventList)
 }
+    
+  
 
 export default EventsIndex
